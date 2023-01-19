4531f8dad5806299a129733f52712093c2a251be
shenxrr <x5shen@ucsd.edu>
2023-01-18 16:44:12 -0800

    Create SearchEngine.java

---
+++ SearchEngine.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.io.IOException;
+import java.net.URI;
+
+class Handler implements URLHandler {
+    // The one bit of state on the server: a number that will be manipulated by
+    // various requests.
+    int num = 0;
+    ArrayList<String> a = new ArrayList<>();
+    public String handleRequest(URI url) {
+        if (url.getPath().equals("/")) {
+            return String.format("Xinran's number: %d", num);
+        } else if (url.getPath().equals("/increment")) {
+            num += 1;
+            return String.format("Number incremented!");
+        } else {
+            System.out.println("Path: " + url.getPath());
+            if (url.getPath().contains("/add")) {
+                String[] parameters = url.getQuery().split("=");
+                a.add(parameters[0]);
+                return String.format("Xinran's words: %s", parameters[0]);
+            }
+            return "404 Not Found!";
+        }
+    }
+}
+
+public class SearchEngine {
+    public static void main(String[] args) throws IOException {
+        if(args.length == 0){
+            System.out.println("Missing port number! Try any number between 1024 to 49151");
+            return;
+        }
+
+        int port = Integer.parseInt(args[0]);
+
+        Server.start(port, new Handler());
+    }
+}
